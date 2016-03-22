d6199a56530eb177fe16db4527d59f0da3420a0d
Marc J. Schmidt <marc@marcjschmidt.de>
2016-03-22 02:28:12 +0100

    Added for Symfony bootstrap more preHandler stuff that resets some objects, especially the profiler stuff.

--- Bootstraps/Symfony.php
+++ Bootstraps/Symfony.php
@@ -2,6 +2,7 @@
 
 namespace PHPPM\Bootstraps;
 
+use PHPPM\Utils;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -59,6 +60,7 @@ class Symfony implements BootstrapInterface, HooksInterface
         $request = new Request();
         $request->setMethod(Request::METHOD_HEAD);
         $app->handle($request);
+        $this->postHandle($app);
 
         return $app;
     }
@@ -71,14 +73,7 @@ class Symfony implements BootstrapInterface, HooksInterface
     public function preHandle($app)
     {
         //resets Kernels startTime, so Symfony can correctly calculate the execution time
-        $func = \Closure::bind(
-            function () {
-                $this->startTime = microtime(true);
-            },
-            $app,
-            'AppKernel'
-        );
-        $func($app);
+        Utils::hijackProperty($app, 'startTime', microtime(true));
     }
 
     /**
@@ -88,15 +83,84 @@ class Symfony implements BootstrapInterface, HooksInterface
      */
     public function postHandle($app)
     {
+        $container = $app->getContainer();
+
         //resets stopwatch, so it can correctly calculate the execution time
-        if ($app->getContainer()->has('debug.stopwatch')) {
-            $app->getContainer()->get('debug.stopwatch')->__construct();
+        if ($container->has('debug.stopwatch')) {
+            $container->get('debug.stopwatch')->__construct();
         }
 
-        if ($app->getContainer()->has('profiler')) {
+        //reset all profiler stuff currently supported
+        if ($container->has('profiler')) {
+            $profiler = $container->get('profiler');
+
             // since Symfony does not reset Profiler::disable() calls after each request, we need to do it,
             // so the profiler bar is visible after the second request as well.
-            $app->getContainer()->get('profiler')->enable();
+            $profiler->enable();
+
+            //PropelLogger
+            if ($container->has('propel.logger')) {
+                $propelLogger = $container->get('propel.logger');
+                Utils::hijackProperty($propelLogger, 'queries', []);
+            }
+
+            //Doctrine
+            //Doctrine\Bundle\DoctrineBundle\DataCollector\DoctrineDataCollector
+            if ($profiler->has('db')) {
+                Utils::bindAndCall(function () {
+                    //$logger: \Doctrine\DBAL\Logging\DebugStack
+                    foreach ($this->loggers as $logger){
+                        Utils::hijackProperty($logger, 'queries', []);
+                    }
+                }, $profiler->get('db'), null, 'Symfony\Bridge\Doctrine\DataCollector\DoctrineDataCollector');
+            }
+
+            //ExceptionDataCollector
+            if ($profiler->has('events')) {
+                Utils::hijackProperty($profiler->get('events'), 'data', array(
+                    'called_listeners' => array(),
+                    'not_called_listeners' => array(),
+                ));
+            }
+
+            //TwigDataCollector
+            if ($profiler->has('twig')) {
+                Utils::bindAndCall(function () {
+                    Utils::hijackProperty($this->profile, 'profiles', []);
+                }, $profiler->get('twig'));
+            }
+
+            //Logger
+            if ($container->has('logger')) {
+                $logger = $container->get('logger');
+                Utils::bindAndCall(function () {
+                    if ($debugLogger = $this->getDebugLogger()) {
+                        //DebugLogger
+                        Utils::hijackProperty($debugLogger, 'records', []);
+                    }
+                }, $logger);
+            }
+
+            //SwiftMailer logger
+            //Symfony\Bundle\SwiftmailerBundle\DataCollector\MessageDataCollector
+            if ($container->hasParameter('swiftmailer.mailers')) {
+                $mailers = $container->getParameter('swiftmailer.mailers');
+                foreach ($mailers as $name => $mailer) {
+                    $loggerName = sprintf('swiftmailer.mailer.%s.plugin.messagelogger', $name);
+                    if ($container->has($loggerName)) {
+                        /** @var \Swift_Plugins_MessageLogger $logger */
+                        $logger = $container->get($loggerName);
+                        $logger->clear();
+                    }
+                }
+            }
+
+            //Symfony\Bridge\Swiftmailer\DataCollector\MessageDataCollector
+            if ($container->has('swiftmailer.plugin.messagelogger')) {
+                $logger = $container->get('swiftmailer.plugin.messagelogger');
+                $logger->clear();
+            }
         }
+
     }
 }
