7607667088eee7f0a6e67ff4f9b1a38f972811f8
Niklas Keller <me@kelunik.com>
2017-09-04 13:00:10 +0200

    Rewrite based on Aerys

--- Bridges/HttpKernel.php
+++ Bridges/HttpKernel.php
@@ -2,14 +2,17 @@
 
 namespace PHPPM\Bridges;
 
+use function Amp\call;
+use Amp\Promise;
+use Amp\Success;
+use Amp\Uri\Uri;
+use Aerys\Request;
+use Aerys\Response;
 use PHPPM\Bootstraps\ApplicationEnvironmentAwareInterface;
 use PHPPM\Bootstraps\BootstrapInterface;
 use PHPPM\Bootstraps\HooksInterface;
 use PHPPM\Bootstraps\RequestClassProviderInterface;
-use PHPPM\React\HttpResponse;
 use PHPPM\Utils;
-use React\EventLoop\LoopInterface;
-use React\Http\Request as ReactRequest;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
@@ -19,7 +22,7 @@ use Symfony\Component\HttpKernel\TerminableInterface;
 class HttpKernel implements BridgeInterface
 {
     /**
-     * An application implementing the HttpKernelInterface
+     * An application implementing the HttpKernelInterface.
      *
      * @var \Symfony\Component\HttpKernel\HttpKernelInterface
      */
@@ -36,16 +39,17 @@ class HttpKernel implements BridgeInterface
      * In the process of bootstrapping we decorate our application with any number of
      * *middlewares* using StackPHP's Stack\Builder.
      *
-     * The app bootstraping itself is actually proxied off to an object implementing the
+     * The app bootstrapping itself is actually proxied off to an object implementing the
      * PHPPM\Bridges\BridgeInterface interface which should live within your app itself and
      * be able to be autoloaded.
      *
      * @param string $appBootstrap The name of the class used to bootstrap the application
      * @param string|null $appenv The environment your application will use to bootstrap (if any)
      * @param boolean $debug If debug is enabled
+     *
      * @see http://stackphp.com
      */
-    public function bootstrap($appBootstrap, $appenv, $debug, LoopInterface $loop)
+    public function bootstrap($appBootstrap, $appenv, $debug)
     {
         $appBootstrap = $this->normalizeAppBootstrap($appBootstrap);
 
@@ -61,79 +65,69 @@ class HttpKernel implements BridgeInterface
     /**
      * {@inheritdoc}
      */
-    public function getStaticDirectory()
+    public function getStaticDirectory(): string
     {
         return $this->bootstrap->getStaticDirectory();
     }
 
-    /**
-     * Handle a request using a HttpKernelInterface implementing application.
-     *
-     * @param ReactRequest $request
-     * @param HttpResponse $response
-     *
-     * @throws \Exception
-     */
-    public function onRequest(ReactRequest $request, HttpResponse $response)
+    /** @inheritdoc */
+    public function onRequest(Request $request, Response $response): Promise
     {
         if (null === $this->application) {
-            return;
+            return new Success;
         }
 
-        $syRequest = $this->mapRequest($request);
+        return call(function () use ($request, $response) {
+            $syRequest = yield from $this->mapRequest($request);
 
-        // start buffering the output, so cgi is not sending any http headers
-        // this is necessary because it would break session handling since
-        // headers_sent() returns true if any unbuffered output reaches cgi stdout.
-        ob_start();
+            // start buffering the output, so cgi is not sending any http headers
+            // this is necessary because it would break session handling since
+            // headers_sent() returns true if any unbuffered output reaches cgi stdout.
+            ob_start();
 
-        try {
-            if ($this->bootstrap instanceof HooksInterface) {
-                $this->bootstrap->preHandle($this->application);
+            try {
+                if ($this->bootstrap instanceof HooksInterface) {
+                    $this->bootstrap->preHandle($this->application);
+                }
+
+                $syResponse = $this->application->handle($syRequest);
+            } catch (\Exception $exception) {
+                $response->setStatus(500); // internal server error
+                $response->end();
+
+                // end buffering if we need to throw
+                @ob_end_clean();
+
+                throw $exception;
             }
 
-            $syResponse = $this->application->handle($syRequest);
-        } catch (\Exception $exception) {
-            $response->writeHead(500); // internal server error
-            $response->end();
-
-            // end buffering if we need to throw
+            // should not receive output from application->handle()
             @ob_end_clean();
-            throw $exception;
-        }
 
-        // should not receive output from application->handle()
-        @ob_end_clean();
+            $this->mapResponse($response, $syResponse);
 
-        $this->mapResponse($response, $syResponse);
+            if ($this->application instanceof TerminableInterface) {
+                $this->application->terminate($syRequest, $syResponse);
+            }
 
-        if ($this->application instanceof TerminableInterface) {
-            $this->application->terminate($syRequest, $syResponse);
-        }
-
-        if ($this->bootstrap instanceof HooksInterface) {
-            $this->bootstrap->postHandle($this->application);
-        }
+            if ($this->bootstrap instanceof HooksInterface) {
+                $this->bootstrap->postHandle($this->application);
+            }
+        });
     }
 
-    /**
-     * Convert React\Http\Request to Symfony\Component\HttpFoundation\Request
-     *
-     * @param ReactRequest $reactRequest
-     * @return SymfonyRequest $syRequest
-     */
-    protected function mapRequest(ReactRequest $reactRequest)
+    protected function mapRequest(Request $aerysRequest): \Generator
     {
-        $method = $reactRequest->getMethod();
-        $headers = $reactRequest->getHeaders();
-        $query = $reactRequest->getQuery();
+        $method = $aerysRequest->getMethod();
+        $headers = $aerysRequest->getAllHeaders();
+        $query = (new Uri($aerysRequest->getUri()))->getQuery();
 
         $_COOKIE = [];
 
         $sessionCookieSet = false;
 
-        if (isset($headers['Cookie']) || isset($headers['cookie'])) {
-            $headersCookie = explode(';', isset($headers['Cookie']) ? $headers['Cookie'] : $headers['cookie']);
+        foreach ($headers['cookie'] ?? [] as $cookieHeader) {
+            $headersCookie = explode(';', $cookieHeader);
             foreach ($headersCookie as $cookie) {
                 list($name, $value) = explode('=', trim($cookie));
                 $_COOKIE[$name] = $value;
@@ -146,37 +140,33 @@ class HttpKernel implements BridgeInterface
         }
 
         if (!$sessionCookieSet && session_id()) {
-            //session id already set from the last round but not got from the cookie header,
-            //so generate a new one, since php is not doing it automatically with session_start() if session
-            //has already been started.
+            // session id already set from the last round but not got from the cookie header,
+            // so generate a new one, since php is not doing it automatically with session_start() if session
+            // has already been started.
             session_id(Utils::generateSessionId());
         }
 
-        $files = $reactRequest->getFiles();
-        $post = $reactRequest->getPost();
+        $files = []; // TODO: Support files and POST
+        $post = [];
 
         if ($this->bootstrap instanceof RequestClassProviderInterface) {
             $class = $this->bootstrap->requestClass();
+        } else {
+            $class = SymfonyRequest::class;
         }
-        else {
-            $class = '\Symfony\Component\HttpFoundation\Request';
-        }
+
+        \parse_str($query, $queryParams);
+
+        $body = yield $aerysRequest->getBody();
 
         /** @var SymfonyRequest $syRequest */
-        $syRequest = new $class($query, $post, $attributes = [], $_COOKIE, $files, $_SERVER, $reactRequest->getBody());
-
+        $syRequest = new $class($queryParams, $post, $attributes = [], $_COOKIE, $files, $_SERVER, $body);
         $syRequest->setMethod($method);
 
         return $syRequest;
     }
 
-    /**
-     * Convert Symfony\Component\HttpFoundation\Response to React\Http\Response
-     *
-     * @param HttpResponse $reactResponse
-     * @param SymfonyResponse $syResponse
-     */
-    protected function mapResponse(HttpResponse $reactResponse, SymfonyResponse $syResponse)
+    protected function mapResponse(Response $aerysResponse, SymfonyResponse $syResponse)
     {
         // end active session
         if (PHP_SESSION_ACTIVE === session_status()) {
@@ -191,15 +181,7 @@ class HttpKernel implements BridgeInterface
                 $name = substr($header, 0, $pos);
                 $value = trim(substr($header, $pos + 1));
 
-                if (isset($nativeHeaders[$name])) {
-                    if (!is_array($nativeHeaders[$name])) {
-                        $nativeHeaders[$name] = [$nativeHeaders[$name]];
-                    }
-
-                    $nativeHeaders[$name][] = $value;
-                } else {
-                    $nativeHeaders[$name] = $value;
-                }
+                $nativeHeaders[$name][] = $value;
             }
         }
 
@@ -207,7 +189,11 @@ class HttpKernel implements BridgeInterface
         // operates on a clean header.
         header_remove();
 
-        $headers = array_merge($nativeHeaders, $syResponse->headers->allPreserveCase());
+        $headers = array_merge(
+            \array_change_key_case($nativeHeaders, \CASE_LOWER),
+            \array_change_key_case($syResponse->headers->allPreserveCase(), \CASE_LOWER)
+        );
+
         $cookies = [];
 
         /** @var Cookie $cookie */
@@ -235,18 +221,28 @@ class HttpKernel implements BridgeInterface
             $cookies[] = $cookieHeader;
         }
 
-        if (isset($headers['Set-Cookie'])) {
-            $headers['Set-Cookie'] = array_merge((array)$headers['Set-Cookie'], $cookies);
+        if (isset($headers['set-cookie'])) {
+            $headers['set-cookie'] = array_merge((array) $headers['set-cookie'], $cookies);
         } else {
-            $headers['Set-Cookie'] = $cookies;
+            $headers['set-cookie'] = $cookies;
         }
 
         if ($syResponse instanceof SymfonyStreamedResponse) {
-            $reactResponse->writeHead($syResponse->getStatusCode(), $headers);
+            $aerysResponse->setStatus($syResponse->getStatusCode());
+
+            foreach ($headers as $key => $values) {
+                foreach ($values as $i => $value) {
+                    if ($i === 0) {
+                        $aerysResponse->setHeader($key, $value);
+                    } else {
+                        $aerysResponse->addHeader($key, $value);
+                    }
+                }
+            }
 
             // asynchronously get content
-            ob_start(function($buffer) use ($reactResponse) {
-                $reactResponse->write($buffer);
+            ob_start(function($buffer) use ($aerysResponse) {
+                $aerysResponse->write($buffer);
                 return '';
             }, 4096);
 
@@ -254,28 +250,36 @@ class HttpKernel implements BridgeInterface
 
             // flush remaining content
             @ob_end_flush();
-            $reactResponse->end();
-        }
-        else {
+            $aerysResponse->end();
+        } else {
             ob_start();
             $content = $syResponse->getContent();
             @ob_end_flush();
 
-            if (!isset($headers['Content-Length'])) {
-                $headers['Content-Length'] = strlen($content);
+            $aerysResponse->setStatus($syResponse->getStatusCode());
+
+            foreach ($headers as $key => $values) {
+                foreach ($values as $i => $value) {
+                    if ($i === 0) {
+                        $aerysResponse->setHeader($key, $value);
+                    } else {
+                        $aerysResponse->addHeader($key, $value);
+                    }
+                }
             }
 
-            $reactResponse->writeHead($syResponse->getStatusCode(), $headers);
-            $reactResponse->end($content);
+            $aerysResponse->end($content);
         }
     }
 
     /**
      * @param $appBootstrap
+     *
      * @return string
+     *
      * @throws \RuntimeException
      */
-    protected function normalizeAppBootstrap($appBootstrap)
+    protected function normalizeAppBootstrap($appBootstrap): string
     {
         $appBootstrap = str_replace('\\\\', '\\', $appBootstrap);
 
@@ -290,5 +294,7 @@ class HttpKernel implements BridgeInterface
                 return $class;
             }
         }
+
+        throw new \RuntimeException('Class ' . $appBootstrap . ' does not exist');
     }
 }
