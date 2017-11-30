91647cfd39bfcca2d5bc55673c43a0d411e7fe56
Dave Redfern <david.redfern@madagasgar.com>
2017-11-30 14:49:02 -0500

    Re-add Symfony class for BC

---
+++ Bootstraps/Symfony.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHPPM\Bootstraps;
+
+/**
+ * Class Symfony
+ *
+ * BC class.
+ *
+ * @package    PHPPM\Bootstraps
+ * @subpackage PHPPM\Bootstraps\Symfony
+ */
+class Symfony extends SymfonyApp
+{
+
+}
