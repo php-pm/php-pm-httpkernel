101ef924a36b1579237ea6a3cb71f4dfe80024df
Dave Redfern <david.redfern@madagasgar.com>
2017-11-30 14:22:33 -0500

    Adds support for Symfony 3.3+ / 4
    Separates Symfony kernel bootstrap into App and Api

--- Bootstraps/AbstractSymfony.php
+++ Bootstraps/AbstractSymfony.php
@@ -2,15 +2,16 @@
 
 namespace PHPPM\Bootstraps;
 
-use PHPPM\Symfony\StrongerNativeSessionStorage;
 use PHPPM\Utils;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 /**
- * A default bootstrap for the Symfony framework
+ * Shared bootstrap logic for Symfony Kernels. Handles changes introduced in Symfony 3.3/4 and 4.0
  */
-class Symfony implements BootstrapInterface, HooksInterface, ApplicationEnvironmentAwareInterface
+abstract class AbstractSymfony implements BootstrapInterface, HooksInterface, ApplicationEnvironmentAwareInterface
 {
+
     /**
      * @var string|null The application environment
      */
@@ -34,58 +35,9 @@ class Symfony implements BootstrapInterface, HooksInterface, ApplicationEnvironm
     }
 
     /**
-     * Create a Symfony application
-     *
-     * @return \AppKernel
-     */
-    public function getApplication()
-    {
-        // include applications autoload
-        $appAutoLoader = './app/autoload.php';
-        if (file_exists($appAutoLoader)) {
-            require $appAutoLoader;
-        } else {
-            require './vendor/autoload.php';
-        }
-
-        //since we need to change some services, we need to manually change some services
-        $app = new \AppKernel($this->appenv, $this->debug);
-        // We need to change some services, before the boot, because they would 
-        // otherwise be instantiated and passed to other classes which makes it 
-        // impossible to replace them.
-
-        Utils::bindAndCall(function() use ($app) {
-            // init bundles
-            $app->initializeBundles();
-
-            // init container
-            $app->initializeContainer();
-        }, $app);
-
-        //now we can modify the container
-        $nativeStorage = new StrongerNativeSessionStorage(
-            $app->getContainer()->getParameter('session.storage.options'),
-            $app->getContainer()->has('session.handler') ? $app->getContainer()->get('session.handler'): null,
-            $app->getContainer()->get('session.storage.metadata_bag')
-        );
-        $app->getContainer()->set('session.storage.native', $nativeStorage);
-
-        Utils::bindAndCall(function() use ($app) {
-            foreach ($app->getBundles() as $bundle) {
-                $bundle->setContainer($app->container);
-                $bundle->boot();
-            }
-
-            $app->booted = true;
-        }, $app);
-
-        return $app;
-    }
-
-    /**
      * Does some necessary preparation before each request.
      *
-     * @param \AppKernel $app
+     * @param KernelInterface $app
      */
     public function preHandle($app)
     {
@@ -96,7 +48,7 @@ class Symfony implements BootstrapInterface, HooksInterface, ApplicationEnvironm
     /**
      * Does some necessary clean up after each request.
      *
-     * @param \AppKernel $app
+     * @param KernelInterface $app
      */
     public function postHandle($app)
     {
@@ -189,6 +141,84 @@ class Symfony implements BootstrapInterface, HooksInterface, ApplicationEnvironm
                 $logger->clear();
             }
         }
+    }
 
+    /**
+     * @return KernelInterface
+     */
+    protected function createKernelInstance()
+    {
+        // include applications autoload
+        require './vendor/autoload.php';
+
+        // attempt to preload the environment vars
+        $this->loadEnvironmentVariables();
+
+        // locate and attempt to boot the kernel in the current project folder
+        $kernel = $this->locateApplicationKernel();
+
+        return new $kernel($this->appenv, $this->debug);
+    }
+
+    /**
+     * @param KernelInterface $app
+     */
+    protected function initializeKernel($app)
+    {
+        // we need to change some services, before the boot, because they would otherwise
+        // be instantiated and passed to other classes which makes it impossible to replace them.
+        Utils::bindAndCall(function () use ($app) {
+            $app->initializeBundles();
+            $app->initializeContainer();
+        }, $app);
+    }
+
+    /**
+     * @param KernelInterface $app
+     */
+    protected function bootKernel($app)
+    {
+        Utils::bindAndCall(function () use ($app) {
+            foreach ($app->getBundles() as $bundle) {
+                $bundle->setContainer($app->container);
+                $bundle->boot();
+            }
+
+            $app->booted = true;
+        }, $app);
+    }
+
+    /**
+     * Attempt to load the env vars from .env, only if Dotenv exists
+     */
+    protected function loadEnvironmentVariables()
+    {
+        if (!getenv('APP_ENV') && class_exists('Symfony\Component\Dotenv\Dotenv')) {
+            (new \Symfony\Component\Dotenv\Dotenv())->load(realpath('./.env'));
+        }
+    }
+
+    /**
+     * Based on getNamespace from Illuminate\Foundation\Application
+     *
+     * @return string
+     */
+    protected function locateApplicationKernel()
+    {
+        $composer = json_decode(file_get_contents(realpath('./composer.json')), true);
+
+        if (!isset($composer['autoload']['psr-4'])) {
+            return 'AppKernel';
+        }
+
+        foreach ((array) $composer['autoload']['psr-4'] as $namespace => $path) {
+            foreach ((array) $path as $pathChoice) {
+                if (realpath('./src/') == realpath('./'.$pathChoice)) {
+                    return $namespace . 'Kernel';
+                }
+            }
+        }
+
+        return 'AppKernel';
     }
 }
