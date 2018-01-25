3fc2c279f373ad1f23a32ed955088c8bb0927197
Cyril Mizzi <cmizzi@users.noreply.github.com>
2018-01-25 15:31:30 +0100

    Fix laravel session handling (#85)

--- Bridges/HttpKernel.php
+++ Bridges/HttpKernel.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse as SymfonyStreamedResponse;
 use Symfony\Component\HttpKernel\TerminableInterface;
+use Illuminate\Contracts\Http\Kernel;
 
 
 class HttpKernel implements BridgeInterface
@@ -104,6 +105,10 @@ class HttpKernel implements BridgeInterface
         $response = $this->mapResponse($syResponse);
 
         if ($this->application instanceof TerminableInterface) {
+            $this->application->terminate($syRequest, $syResponse);
+        }
+        
+        if ($this->application instanceof Kernel) {
             $this->application->terminate($syRequest, $syResponse);
         }
 
---
+++ Laravel/SessionGuard.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace PHPPM\Laravel;
+
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Foundation\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+class SessionGuard extends \Illuminate\Auth\SessionGuard
+{
+
+    /**
+     * App instance
+     *
+     * @var mixed|\Illuminate\Foundation\Application $app
+     */
+    protected $app;
+
+    /**
+     * Create a new authentication guard.
+     *
+     * @param  string  $name
+     * @param  \Illuminate\Contracts\Auth\UserProvider  $provider
+     * @param  \Symfony\Component\HttpFoundation\Session\SessionInterface  $session
+     * @param  \Symfony\Component\HttpFoundation\Request  $request
+     * @param  mixed|\Illuminate\Foundation\Application $app
+     * @return void
+     */
+    public function __construct($name,
+        UserProvider $provider,
+        SessionInterface $session,
+        Request $request = null,
+        Application $app)
+    {
+        $this->name = $name;
+        $this->session = $session;
+        $this->request = $request;
+        $this->provider = $provider;
+        $this->app = $app;
+    }
+
+    /**
+     * Set the current request instance.
+     *
+     * @param  \Symfony\Component\HttpFoundation\Request  $request
+     * @return $this
+     */
+    public function setRequest(Request $request)
+    {
+        // reset the current state
+        $this->reset();
+
+        // retrieve a new session from the app
+        $this->session = $this->app->make('session');
+
+        return parent::setRequest($request);
+    }
+
+    /**
+     * Reset the state of current class instance.
+     *
+     * @return void
+     */
+    protected function reset()
+    {
+        $this->user = null;
+        $this->lastAttempted = null;
+        $this->viaRemember = false;
+        $this->loggedOut = false;
+        $this->tokenRetrievalAttempted = false;
+    }
+}
