ce4e927a0093b4298a9a789d75d40a73818bdbb7
Chris Thomas <christopher.thomas@plista.com>
2018-08-09 15:27:15 +0200

    fix the problem with the custom namespaces for the symfony kernel

--- Bootstraps/Symfony.php
+++ Bootstraps/Symfony.php
@@ -54,7 +54,9 @@ class Symfony implements BootstrapInterface, HooksInterface, ApplicationEnvironm
             (new \Symfony\Component\Dotenv\Dotenv())->load(realpath('.env'));
         }
 
-        $class = class_exists('\AppKernel') ? '\AppKernel' : '\App\Kernel';
+	    $namespace = getenv('APP_KERNEL_NAMESPACE') ?: '\App\\';
+	    $fqcn = $namespace . (getenv('APP_KERNEL_CLASS_NAME') ?: 'Kernel');
+	    $class = class_exists($fqcn) ? $fqcn : '\AppKernel';
 
         //since we need to change some services, we need to manually change some services
         $app = new $class($this->appenv, $this->debug);
